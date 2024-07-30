abb88422ed88d717135e572e4f58d525227fda14
lucasFreitasDev97 <lukasm2@live.com>
2024-07-30 19:11:13 -0300

    prepare itens to menu

--- resources/views/templates/partials/menu.blade.php
+++ resources/views/templates/partials/menu.blade.php
@@ -13,31 +13,31 @@
     <ul class="menu-inner py-1">
         <!-- Dashboards -->
         <li class="menu-item">
-            <a href="" class="menu-link d-flex">
+            <a href="#" class="menu-link d-flex">
                 <i class="menu-icon tf-icons mdi mdi-home-outline"></i>
                 <div>Home Page</div>
             </a>
         </li>
         <li class="menu-item">
-            <a href="" class="menu-link d-flex">
+            <a href="#" class="menu-link d-flex">
                 <i class="menu-icon tf-icons mdi mdi-database-outline"></i>
                 <div>Products</div>
             </a>
         </li>
         <li class="menu-item">
-            <a href="" class="menu-link d-flex">
+            <a href="#" class="menu-link d-flex">
                 <i class="menu-icon tf-icons mdi mdi-truck-outline"></i>
                 <div>Suppliers</div>
             </a>
         </li>
         <li class="menu-item">
-            <a href="" class="menu-link d-flex">
+            <a href="#" class="menu-link d-flex">
                 <i class="menu-icon tf-icons mdi mdi-account-multiple-outline"></i>
                 <div>Customers</div>
             </a>
         </li>
         <li class="menu-item">
-            <a href="" class="menu-link d-flex">
+            <a href="#" class="menu-link d-flex">
                 <i class="menu-icon tf-icons mdi mdi-shopping-outline"></i>
                 <div>Orders</div>
             </a>
